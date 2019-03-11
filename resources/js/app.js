a801c1db31bfc6b03d20410f9eb13e7b90c767b8
William <wtpwinberg@gmail.com>
2019-03-10 22:41:32 -0700

    mast, nav, layout boiler

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+window.Vuetify = require('vuetify');
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,7 +21,11 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
+Vue.use(Vuetify);
+
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('navigation', require('./components/navigation.vue').default);
+Vue.component('mast-nav-layout', require('./components/MastNavLayout.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div class="container">
+    <navigation></navigation>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -19,5 +20,7 @@
             </div>
         </div>
     </div>
+
+
 </div>
 @endsection
